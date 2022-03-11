6632b4a0fb28ba5dd41b96a4f23396d9005bcfcb
Cherrelle-Ward <Cherrelle_Ward@hotmail.co.uk>
2022-03-11 15:19:27 +0000

    final commit

---
+++ app.js
@@ -0,0 +1,58 @@
+// const whichGreeting = (timeOfDay) => {
+//     console.log(`good ${timeOfDay}`);
+// }
+
+// const greet = (time, fn) => {
+//     if(time < 1200) {
+//         fn("Morning")
+//     } else if (time >= 1200 && time < 1800){
+//         fn("Afternoon")
+//     } else {
+//         fn("Evening");
+//     }
+
+// }
+// greet(1400, whichGreeting);
+//////////////////////////////////////////
+
+//  const add = () => {
+//     return 2+3;
+// }
+// console.log(add());
+// console.log(add);
+//////////////////////////////////
+// const thisFunction = () => {
+//     return "Booya";
+// }
+
+// const add = (example) => {
+//     add2(example);
+// }
+
+// const add2 = (example) => {
+//     example();
+// }
+
+// add(thisFunction);
+////////////////////////////////
+
+// const add =  (num1) => {
+//     return (num2) => {
+//         return num1 + num2;
+//     }
+// }
+
+// console.log(add(2)(7));
+
+// (7) is calling the nested function with the value of num2
+/////////////////////////////////////////
+
+// NESTED ARRAY EXAMPLE 
+// let myArray = [
+    //     [1, 2, 3, 4, 5], 
+    //     [6, 7, 8, 9, 10], 
+    //     [11, 12, 13, 14, {name: "Jordan"}]
+    // ];
+    // Object within the array
+
+// console.log(myArray[2][4].name)
